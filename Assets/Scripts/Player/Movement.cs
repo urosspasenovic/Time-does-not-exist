fec7749cbb44e8def0fae2998574127e46ae5cbb
Uros <urosspasenovic9@gmail.com>
2022-09-10 01:37:39 +0200

    Movement with rotation

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -7,59 +7,68 @@ public class Movement : MonoBehaviour
     public bool IsMoving { get; set; } = false;
     [SerializeField]
     LayerMask layerMask;
-    //[SerializeField]
-    //float rotationSpeed;
+    [SerializeField]
+    Transform playerBody;
+    [SerializeField]
+    float rotationSpeed;
     [SerializeField]
     float moveSpeed;
     RaycastHit hit;
-    Rigidbody rigidbody;
     Transform trans;
-    Transform playerBody;
     Vector3 moveToPosition;
-    Vector3 moveInDirection;
+    Quaternion rotateTo;
     private void Awake()
     {
-        trans = base.transform;
-        rigidbody = GetComponent<Rigidbody>();
-        playerBody = base.transform.GetChild(0);
+        trans = transform;
+    }
+    private void FixedUpdate()
+    {
+        if (IsMoving)
+        {           
+            trans.position = Vector3.MoveTowards(trans.position, moveToPosition, moveSpeed * Time.deltaTime);
+            if(Vector3.Distance(playerBody.position, moveToPosition) < 0.2f)
+                StopMoving();
+            playerBody.rotation =  Quaternion.RotateTowards(playerBody.rotation, rotateTo, rotationSpeed * Time.deltaTime);          
+
+        }
     }
     void Update()
     {
-        if (IsMoving)
-        {
-            trans.Translate(moveSpeed * Time.deltaTime * moveInDirection);
-            return;
-        }
+        if (IsMoving) return;
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (Physics.Raycast(trans.position, Vector3.left, out hit, 2f, layerMask))
             {
-                MoveToPosition(hit.transform.position, Vector3.left);
-                //playerBody.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-90, 90, 0), Time.deltaTime * rotationSpeed);
+                MoveToPosition(hit.transform.position, Vector3.left);               
+                rotateTo = Quaternion.Euler(-90f, 90f, 0);
+                return;
             }
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             if (Physics.Raycast(trans.position, Vector3.forward, out hit, 2f, layerMask))
             {
-                MoveToPosition(hit.transform.position, Vector3.forward);
-                //playerBody.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-90, 90, 0), Time.deltaTime * rotationSpeed);
+                MoveToPosition(hit.transform.position, Vector3.forward);               
+                rotateTo = Quaternion.Euler(-90f, 180f, 0);
+                return;
             }
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             if (Physics.Raycast(trans.position, Vector3.right, out hit, 2f, layerMask))
             {
-                MoveToPosition(hit.transform.position, Vector3.right);
-                //playerBody.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-90, 90, 0), Time.deltaTime * rotationSpeed);
+                MoveToPosition(hit.transform.position, Vector3.right);             
+                rotateTo = Quaternion.Euler(-90f, 270f, 0);
+                return;
             }
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             if (Physics.Raycast(trans.position, Vector3.back, out hit, 2f, layerMask))
             {
-                MoveToPosition(hit.transform.position, Vector3.back);
-                //playerBody.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-90, 90, 0), Time.deltaTime * rotationSpeed);
+                MoveToPosition(hit.transform.position, Vector3.back);              
+                rotateTo = Quaternion.Euler(-90f, 0, 0);
+                return;
             }
         }
     }
@@ -70,8 +79,12 @@ public class Movement : MonoBehaviour
     }
     private void MoveToPosition(Vector3 endPosition, Vector3 direction)
     {
-        moveInDirection = direction;
         IsMoving = true;
         moveToPosition = endPosition;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Plane"))
+            Destroy(other.gameObject);
     }
 }
